60abf2c066e59b2b9b6a63c0545e2ed7099300da
divyaranat <divyaranat@gmail.com>
2022-06-09 20:56:15 -0700

    Make letToNumHMap final

--- src/letterToNumberHashMap.java
+++ src/letterToNumberHashMap.java
@@ -2,17 +2,15 @@ import java.util.HashMap;
 
 public class letterToNumberHashMap {
 
-	private static HashMap<String, Integer> letToNumHMap = new HashMap<>();
+	private static final HashMap<String, Integer> letToNumHMap = new HashMap<>();
 
 	public static void activateLetterToNumberHashMap(){
 		for(int i = 0, j = 65; i <= 25; i++, j++){
 			letToNumHMap.put(Character.toString((char)j), i);
 		}
-
 	}
 
 	public static int letterToNumber(String letter){
-		System.out.println("h " + letter);
 		return letToNumHMap.get(letter);
 	}
 }
