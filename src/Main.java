8cff7cbbf180bf081e1d2f780a332fdec1d0d1e6
divyaranat <divyaranat@gmail.com>
2022-06-11 18:48:58 -0700

    Fix infinite loop in menu()

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,6 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
 		Keyboard.rotorSelectionPrompt(scnr);
-		letterToNumberHashMap.activateLetterToNumberHashMap();
 		menu(scnr);
 	}
 
@@ -13,12 +12,18 @@ public class Main {
 		char selection;
 		do{
 			Keyboard.messageInput(scnr);
+			Keyboard.encryptionMessage();
 			Keyboard.launchToRotor();
+			System.out.println();
 			System.out.println("-MENU-");
 			System.out.println("'N': Encrypt a new message.");
 			System.out.println("'Q': To Quit.");
 			System.out.print("Selection: ");
 			selection = scnr.nextLine().charAt(0);
-		} while(selection != 'Q' || selection != 'q');
+			if(selection == 'q' || selection == 'Q'){
+				break;
+			}
+			System.out.println();
+		} while(true);
 	}
 }
