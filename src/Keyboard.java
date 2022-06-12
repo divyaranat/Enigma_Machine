94d941e18215e09e7ecca9ebb81f52b9193e32ac
divyaranat <divyaranat@gmail.com>
2022-06-11 18:48:30 -0700

    Add encryptionMessage() and error handling for rotorSelectionPrompt()

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Keyboard {
@@ -8,14 +9,32 @@ public class Keyboard {
 		System.out.println("Pick three rotors to use in machine.");
 		System.out.println("Options: I II III IV V");
 
-		System.out.println("First rotor: ");
+		System.out.print("First rotor: ");
 		String rotorPositionOne = scnr.nextLine();
-		System.out.println("Second rotor: ");
-		String rotorPositionTwo = scnr.nextLine();
-		System.out.println("Third rotor: ");
-		String rotorPositionThree = scnr.nextLine();
+		rotorPositionOne = rotorPositionOne.toUpperCase();
+
+		String rotorPositionTwo;
+		do {
+			System.out.print("Second rotor: ");
+			rotorPositionTwo = scnr.nextLine();
+			rotorPositionTwo = rotorPositionTwo.toUpperCase();
+			if(rotorPositionOne.equals(rotorPositionTwo)){
+				System.out.println("Rotor " + rotorPositionTwo + " has already been selected. Please choose a different rotor.");
+			}
+		} while(rotorPositionTwo.equals(rotorPositionOne));
+
+		String rotorPositionThree;
+		do {
+			System.out.print("Third rotor: ");
+			rotorPositionThree = scnr.nextLine();
+			rotorPositionThree = rotorPositionThree.toUpperCase();
+			if(rotorPositionThree.equals(rotorPositionTwo) || rotorPositionThree.equals(rotorPositionOne)){
+				System.out.println("Rotor " + rotorPositionThree + " has already been selected. Please choose a different rotor.");
+			}
+		} while(rotorPositionThree.equals(rotorPositionOne) || rotorPositionThree.equals(rotorPositionTwo));
 
 		System.out.println("Rotor selection complete.");
+		System.out.println();
 
 		RotorSettings.rotorSelection(rotorPositionOne, rotorPositionTwo, rotorPositionThree);
 	}
@@ -36,12 +55,33 @@ public class Keyboard {
 				messageInput(scnr);
 			}
 		}
+		System.out.println();
+	}
+
+	public static void encryptionMessage(){
+		System.out.println("Encrypting Message... Please Stand By.");
+		System.out.print("[");
+		for(int i = 0; i < 25; i++){
+			System.out.print("*");
+			try{
+				Thread.sleep(100);
+			}
+			catch(InterruptedException ex){
+				Thread.currentThread().interrupt();
+			}
+		}
+		System.out.print("]");
+		System.out.println();
+		System.out.println("Encryption Complete.");
+		System.out.println();
+		System.out.print("Encrypted Message: ");
 	}
 
 	public static void launchToRotor(){
 		for(int i = 0; i < spacelessMessage.length(); i++){
 			String letterMessage = spacelessMessage.substring(i, i+1);
-			RotorFunctions.traverseRotors(letterMessage);
+			System.out.print(RotorFunctions.traverseRotors(letterMessage));
 		}
+		System.out.println();
 	}
 }
